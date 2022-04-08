6ead3aa4f62307abf97310c54a3edfa02c8747b1
zaid <zoe.abedi@gmail.com>
2022-04-08 15:10:59 +0530

    initial commit

---
+++ na.py
@@ -0,0 +1,248 @@
+# Creation of Linked list
+
+class Node:
+   def __init__(self, dataval=None):
+      self.dataval = dataval
+      self.nextval = None
+
+class SLinkedList:
+   def __init__(self):
+      self.headval = None
+
+list1 = SLinkedList()
+list1.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+# Link first Node to second node
+list1.headval.nextval = e2
+
+# Link second Node to third node
+e2.nextval = e3
+
+
+
+
+
+# Traversing a Linked List
+class Node:
+   def __init__(self, dataval=None):
+      self.dataval = dataval
+      self.nextval = None
+
+class SLinkedList:
+   def __init__(self):
+      self.headval = None
+
+   def listprint(self):
+      printval = self.headval
+      while printval is not None:
+         print (printval.dataval)
+         printval = printval.nextval
+
+list = SLinkedList()
+list.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+
+# Link first Node to second node
+list.headval.nextval = e2
+
+# Link second Node to third node
+e2.nextval = e3
+
+list.listprint()
+
+
+# Output
+# Mon
+# Tue
+# Wed
+
+# Insertion in a Linked List
+
+# Inserting At The Beginning
+
+class Node:
+   def __init__(self, dataval=None):
+      self.dataval = dataval
+      self.nextval = None
+
+class SLinkedList:
+   def __init__(self):
+      self.headval = None
+# Print the linked list
+   def listprint(self):
+      printval = self.headval
+      while printval is not None:
+         print (printval.dataval)
+         printval = printval.nextval
+   def AtBegining(self,newdata):
+      NewNode = Node(newdata)
+
+# Update the new nodes next val to existing node
+   NewNode.nextval = self.headval
+   self.headval = NewNode
+
+list = SLinkedList()
+list.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+
+list.headval.nextval = e2
+e2.nextval = e3
+
+list.AtBegining("Sun")
+list.listprint()
+
+# Output
+
+# Sun
+# Mon
+# Tue
+# Wed
+
+# Inserting at the End
+
+class Node:
+   def __init__(self, dataval=None):
+      self.dataval = dataval
+      self.nextval = None
+class SLinkedList:
+   def __init__(self):
+      self.headval = None
+# Function to add newnode
+   def AtEnd(self, newdata):
+      NewNode = Node(newdata)
+      if self.headval is None:
+         self.headval = NewNode
+         return
+      laste = self.headval
+      while(laste.nextval):
+         laste = laste.nextval
+      laste.nextval=NewNode
+# Print the linked list
+   def listprint(self):
+      printval = self.headval
+      while printval is not None:
+         print (printval.dataval)
+         printval = printval.nextval
+
+list = SLinkedList()
+list.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+
+list.headval.nextval = e2
+e2.nextval = e3
+
+list.AtEnd("Thu")
+
+list.listprint()
+
+# Output
+# Mon
+# Tue
+# Wed
+# Thu
+
+# Inserting in between two Data Nodes
+
+class Node:
+   def __init__(self, dataval=None):
+      self.dataval = dataval
+      self.nextval = None
+class SLinkedList:
+   def __init__(self):
+      self.headval = None
+
+# Function to add node
+   def Inbetween(self,middle_node,newdata):
+      if middle_node is None:
+         print("The mentioned node is absent")
+         return
+
+      NewNode = Node(newdata)
+      NewNode.nextval = middle_node.nextval
+      middle_node.nextval = NewNode
+
+# Print the linked list
+   def listprint(self):
+      printval = self.headval
+      while printval is not None:
+         print (printval.dataval)
+         printval = printval.nextval
+
+list = SLinkedList()
+list.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Thu")
+
+list.headval.nextval = e2
+e2.nextval = e3
+
+list.Inbetween(list.headval.nextval,"Fri")
+
+list.listprint()
+
+# Output
+
+# Mon
+# Tue
+# Fri
+# Thu
+
+# Removing an Item
+
+class Node:
+   def __init__(self, data=None):
+      self.data = data
+      self.next = None
+class SLinkedList:
+   def __init__(self):
+      self.head = None
+
+   def Atbegining(self, data_in):
+      NewNode = Node(data_in)
+      NewNode.next = self.head
+      self.head = NewNode
+
+# Function to remove node
+   def RemoveNode(self, Removekey):
+      HeadVal = self.head
+         
+      if (HeadVal is not None):
+         if (HeadVal.data == Removekey):
+            self.head = HeadVal.next
+            HeadVal = None
+            return
+      while (HeadVal is not None):
+         if HeadVal.data == Removekey:
+            break
+         prev = HeadVal
+         HeadVal = HeadVal.next
+
+      if (HeadVal == None):
+         return
+
+      prev.next = HeadVal.next
+         HeadVal = None
+
+   def LListprint(self):
+      printval = self.head
+      while (printval):
+         print(printval.data),
+         printval = printval.next
+
+llist = SLinkedList()
+llist.Atbegining("Mon")
+llist.Atbegining("Tue")
+llist.Atbegining("Wed")
+llist.Atbegining("Thu")
+llist.RemoveNode("Tue")
+llist.LListprint()
+
+
+# Output
+# Thu
+# Wed
+# Mon
